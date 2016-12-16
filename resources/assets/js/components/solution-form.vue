993fa4e08f0a5ae229bc86d0b91922bff488a8f9
Tudor Popescu <tudor.popescu@bhdevelopment.ro>
2016-12-16 21:10:42 +0200

    Working on solutions

---
+++ resources/assets/js/components/solution-form.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <!-- Body -->
+        <div class="form-group" :class="{'has-error': form.errors.has('body')}">
+            <textarea class="form-control" id="body" name="body" v-model="form.body" required="required">
+            </textarea>
+            <span class="help-block" v-show="form.errors.has('body')">
+                @{{ form.errors.get('body') }}
+            </span>
+        </div>
+    </div>
+</template>
+<style>
+</style>
+<script lang="javascript">
+    export default{
+        data(){
+            return {
+                form: new Form({
+                    body: ''
+                })
+            }
+        },
+    }
+
+
+</script>
--- resources/views/pages/problem.blade.php
+++ resources/views/pages/problem.blade.php
@@ -3,13 +3,19 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <h1>{{$problem->title}}</h1>
+            <h3>{{$problem->title}}</h3>
             <p>{{$problem->description}}</p>
-            @if(!in_array($problem->id,\Auth::user()->solved->pluck('id')->toArray()))
+            @if(!Auth::check())
+            @elseif(!in_array($problem->id,\Auth::user()->solved->pluck('id')->toArray()))
                 <answer-form problem-id="{{$problem->id}}" class="pull-right"></answer-form>
             @else
                 <div class="alert alert-info">You have already solved this problem</div>
             @endif
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <solutions-list></solutions-list>
         </div>
     </div>
 @endsection
