b097cf08bd5f38ff9491130682815698e4e15f25
ladyblue94 <nicuandreeaelena@yahoo.com>
2016-12-19 02:28:13 +0200

    formlarul de pe paginade contact
    mai am sa fac after contact :) sa ajunga emailul la noi

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -3,8 +3,38 @@
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/default.min.css">
 @endsection
 @section('content')
-    Contacteaza-ne <br>
-
+    Contacteaza-ne <br> <br><center>
+<form class="form-horizontal" role="form" method="post" action="thankyoupage.php">
+	<div class="form-group">
+		<label for="name" class="col-sm-2 control-label">Name</label>
+		<div class="col-sm-10">
+			<input type="text" class="form-control" id="name" name="name" value="">
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="email" class="col-sm-2 control-label">Email</label>
+		<div class="col-sm-10">
+			<input type="email" class="form-control" id="email" name="email" placeholder="email@domain.com" value="">
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="message" class="col-sm-2 control-label">Message</label>
+		<div class="col-sm-10">
+			<textarea class="form-control" rows="4" name="message"></textarea>
+		</div>
+	</div>
+	
+	<div class="form-group">
+		<div class="col-sm-10 col-sm-offset-2">
+			<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-sm-10 col-sm-offset-2">
+			<! Will be used to display an alert to the user>
+		</div>
+	</div>
+</form></center>
 Ne poti gasi si pe
 
 <a target="_blank" title="follow us on twitter" href="http://www.twitter.com/PLACEHOLDER"><img alt="follow me on twitter" src="//login.create.net/images/icons/user/twitter-b_30x30.png" border=0></a>
