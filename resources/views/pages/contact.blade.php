4a02ee7c0a50d2fb46f81ea955b3ce13e490c8ee
ladyblue94 <nicuandreeaelena@yahoo.com>
2016-12-19 02:07:33 +0200

    am populat pagina de contact
    am modificat si url-ul de facebook cu pagina noastra :)

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -3,7 +3,15 @@
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/default.min.css">
 @endsection
 @section('content')
-    
+    Contacteaza-ne <br>
+
+Ne poti gasi si pe
+
+<a target="_blank" title="follow us on twitter" href="http://www.twitter.com/PLACEHOLDER"><img alt="follow me on twitter" src="//login.create.net/images/icons/user/twitter-b_30x30.png" border=0></a>
+<a target="_blank" title="find us on Facebook" href="https://www.facebook.com/smartaskromania/"><img alt="follow me on facebook" src="//login.create.net/images/icons/user/facebook_30x30.png" border=0></a>
+<a target="_blank" title="follow us on youtube" href="http://www.youtube.com/PLACEHOLDER"><img alt="follow me on youtube" src="https://c866088.ssl.cf3.rackcdn.com/assets/youtube30x30.png" border=0></a>
+<a target="_blank" title="follow us on google plus" href="https://plus.google.com/PLACEHOLDER"><img alt="follow me on google plus" src="https://c866088.ssl.cf3.rackcdn.com/assets/googleplus30x30.png" border=0></a>
+<a target="_blank" title="follow us on pinterest" href="http://www.pinterest.com/PLACEHOLDER"><img alt="follow me on pinterest" src="https://c866088.ssl.cf3.rackcdn.com/assets/pinterest30x30.png" border=0></a>
 @endsection
 @section('sidebar')
     Anything in sidebar goes here
