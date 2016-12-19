74bb32a976f4e15fdc21d375a5f53ca3a26a0a5c
ladyblue94 <nicuandreeaelena@yahoo.com>
2016-12-20 00:14:55 +0200

    privacy_policy

--- app/thankyoupage.php
+++ app/thankyoupage.php
@@ -8,9 +8,7 @@ class thankyoupage extends Model
 { public function index()
     {
         return view('pages.thankyoupage');
-		 if($_POST["message"]) {
-    mail("nicuandreeaelena@yahoo.com", "Form to email message", $_POST["message"], "From: an@email.address");
-}
+		 
     }
 
    
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app-full')
 @section('content')
-    <h1>CONTENT</h1>
+    <h1>  Welcome to SmartAsk!</h1>
 	<br>
-	Welcome to SmartAsk! 
-	
-
+	<h3>  That's the place where everybody can found answers.</h3>
+	<br>
+	 <br>
+	<img src="http://elfhq.com/wp-content/uploads/2013/10/santahuh08.jpg" alt="No image">
+	<br><br><br><br>
+ <hr>
 <div class="cd-form-wrapper cd-container">
-	<form class="cd-form"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<form class="cd-form">
 		<label class="cd-label" for="cd-email">Subscribe</label>
 		<input type="email" id="cd-email" class="cd-email" name="cd-email" placeholder="Enter your email address">
 		<input type="submit" class="cd-submit" value="Submit">
@@ -15,5 +18,7 @@
  
 </div>
 	
+	 <center><a href="https://docs.google.com/document/d/1bLAzkcCbQLpd1Jjb90k1EPG1pWY414T_VeoYR8ajbsM/edit?usp=sharing ">Privacy Policy</a>
+	 </center>
 @endsection
 
