0e819c45eeb54ba8be643f568b03959805dd3930
ladyblue94 <nicuandreeaelena@yahoo.com>
2016-12-20 01:26:49 +0200

    adaugare stiri

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -3,7 +3,62 @@
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/default.min.css">
 @endsection
 @section('content')
-     Stiri de ultima ora!
+  
+	 <br><div class="Row">
+   <div class="Column">
+	<h2><b> <a href="http://www.adweek.com/news/technology/instagram-users-can-now-save-other-peoples-photos-later-175110">Instagram Users Can Now Save Other People's Photos for Later</a></b><h2>
+	<center>
+	<img src="http://www.adweek.com/files/imagecache/node_module_16x9/2016_Dec/instagram-saved-photos-hed-2016.png">
+	</center>
+	<h5>The Facebook-owned app has a new bookmark feature that allows users to save posts in an additional private tab within their profile. Starting today, users can press a bookmark button in the lower right-hand corner of a post.
+	</h5>
+	</div>
+    <div class="Column">
+	<h2><b><a href="http://www.adweek.com/news/technology/if-you-tweet-emoji-googles-account-it-will-link-local-search-results-174978">If You Tweet an Emoji at Google's Account, It Will Link to Local Search Results</a>
+	</h2></b>
+	<center>
+	<img src="http://www.adweek.com/files/imagecache/node_module_16x9/2016_Dec/google-emoji-search-hed-2016.png">
+	</center>
+	<br><h5>Google wants to understand the language of emoji. The tech giant today launched a fun feature that lets people tweet emojis at the brand's Twitter account. 
+	</h5>
+	</div>
+    <div class="Column">
+	<h2><b><a href="http://www.adweek.com/news/technology/instagram-users-can-now-and-disable-comments-174976">Instagram Users Can Now Like (and Disable) Comments</a>
+	</h2></b>
+	<center>
+	<img src="http://www.adweek.com/files/imagecache/node_module_16x9/2016_Dec/instagram-help-hed-2016.png">
+	</center>
+	<h5><br> Instagram announced users can now like user comments in an effort to make the medium more positive. And along with adding 'likes' for comments, users will now also have a way to turn off comments altogether to prevent unwanted comments on their photos and videos. <br>
+	
+	</h5>
+	</div>
+</div>
+
+
+<div class="Row">
+ <div class="Column">
+ <h2><b><a href="http://www.bbc.com/news/business-38289079">Could robots be marking your homework?</a>
+	</h2></b>
+	<center>
+	<img src="http://guardianlv.com/wp-content/uploads/2012/08/Acobot-Live-Chat-Robot.jpg">
+	</center>
+	<h5>A student follows lessons on the computer screen, with a Watson-powered robot helper integrated into the system - asking questions about each topic, offering prompts and giving assistance.
+It's like typing messages back and forth - but the person replying is a computer.
+	</h5>
+	</div> 
+	
+	<div class="Column">
+	<h2><b><a href="http://www.bbc.com/news/technology-38216255">Facial scan app wants to get to know you<a><br>
+	</h2></b><center>
+	<br><Br><img src="http://ahmedopeyemi.com/main/wp-content/uploads/2015/08/xcPiSQM.png"></center><br>
+	<h5>Augmented reality app Blippar has been updated to recognise faces as well as objects.
+The app can now identify up to 70,000 public figures by looking at images, video or an actual person.
+	</h5>
+	</div>
+	<div class="Column">
+   </div>
+</div>
+	 
 	 <hr>
 	 <center><a href="https://docs.google.com/document/d/1bLAzkcCbQLpd1Jjb90k1EPG1pWY414T_VeoYR8ajbsM/edit?usp=sharing ">Privacy Policy</a>
 	 </center>
