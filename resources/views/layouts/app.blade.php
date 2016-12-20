3b3bdc7eb61cf7449cd7c36e3d25881563dd0a60
alina dumitru <alina dumitru>
2016-12-20 16:30:23 +0200

    landing page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
     <!-- Scripts -->
     <script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,80 @@
+<img  src="{{URL::asset('images\banner.jpg')}}" alt="">
 @extends('layouts.app-full')
 @section('content')
-    <h1>CONTENT</h1>
-@endsection
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 col-md-offset-6 ">
+                <h1> If you want to be SMART</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 col-md-offset-0 ">
+                Smart Ask is a series of challenging mathematical/computer programming problems that will require more than just mathematical insights to solve. Although mathematics will help you arrive at elegant and efficient methods, the use of a computer and programming skills will be required to solve most problems.
+            </div>
+            <div class="col-md-1 col-md-offset-0 ">
+                <div class="bignumber"> 1 </div>
+                <img  src="{{URL::asset('images\hex-log.jpg')}}" alt="">
+            </div>
+            <div class="col-md-3 col-md-offset-3 ">
+                <h3> ASK more Questions</h3>
+            </div>
 
+        </div>
+        <div class="row">
+            <div class="col-md-1 col-md-offset-3 ">
+                <div class="bignumber"> 2 </div>
+                <img  src="{{URL::asset('images\hex-logd.jpg')}}" alt="">
+            </div>
+            <div class="col-md-3 col-md-offset-0 ">
+                The motivation for starting Project Euler, and its continuation, is to provide a platform for the inquiring mind to delve into unfamiliar areas and learn new concepts in a fun and recreational context.
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 col-md-offset-1 ">
+                Who are the problems aimed at?
+                The intended audience include students for whom the basic curriculum is not feeding their hunger to learn, adults whose background was not primarily mathematics but had an interest in things mathematical, and professionals who want to keep their problem solving and mathematics on the cutting edge.
+            </div>
+            <div class="col-md-1 col-md-offset-0 ">
+                <div class="bignumber"> 3 </div>
+                <img  src="{{URL::asset('images\hex-log.jpg')}}" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-1 col-md-offset-4 ">
+                <div class="bignumber"> 4 </div>
+                <img  src="{{URL::asset('images\hex-logd.jpg')}}" alt="">
+            </div>
+            <div class="col-md-3 col-md-offset-0 ">
+                Can anyone solve the problems?
+                The problems range in difficulty and for many the experience is inductive chain learning. That is, by solving one problem it will expose you to a new concept that allows you to undertake a previously inaccessible problem. So the determined participant will slowly but surely work his/her way through every problem.
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 col-md-offset-3 ">
+                What next?
+                In order to track your progress it is necessary to setup an account and have Cookies enabled. If you already have an account then Login, otherwise please Register – it's completely free!
+            </div>
+            <div class="col-md-1 col-md-offset-0 ">
+                <div class="bignumber"> 5 </div>
+                <img  src="{{URL::asset('images\hex-log.jpg')}}" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-1 col-md-offset-6 ">
+                <div class="bignumber"> 6 </div>
+                <img  src="{{URL::asset('images\hex-logd.jpg')}}" alt="">
+            </div>
+            <div class="col-md-3 col-md-offset-0 ">
+                However, as the problems are challenging then you may wish to view the Problems before registering.
+                "Project Euler exists to encourage, challenge, and develop the skills and enjoyment of anyone with an interest in the fascinating world of mathematics."
+            </div>
+        </div>
+    </div>
+@endsection
+<!--
+@section('css')
+
+@endsection
+-->
