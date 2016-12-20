77989f7be927468dca91ad4e1011dbee30648ae9
ladyblue94 <nicuandreeaelena@yahoo.com>
2016-12-20 23:26:33 +0200

    contact

--- app/Http/Controllers/ThankYouPageController.php
+++ app/Http/Controllers/ThankYouPageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\thankyoupage;
 
-class ThankYouPageController extends Controller
+class thankyoupageController extends Controller
 {
     public function index()
     {
--- resources/views/pages/thankyoupage.php
+++ resources/views/pages/thankyoupage.php
@@ -1,16 +1,12 @@
 @extends('layouts.app-full')
-@section('head-scripts')
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/default.min.css">
-@endsection
+
 @section('content')
    Multumim pentru mesaj!<br>
    Veti fi contactat in scurt timp! <Br>
    <Br>
    
    @endsection
-@section('sidebar')
-  
-@endsection
+
 @section('end-scripts')
   
    
