59a9619eedea79e9386d6537955c2f1ea454cbd7
Tudor Popescu <tudor.popescu@bhdevelopment.ro>
2017-02-09 23:37:57 +0200

    Add problem seeder

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Problem;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $userAdmin = factory(App\User::class)->create([
+            'email'=>'admin@example.com',
+            'password' => bcrypt('admindemo')
+        ]);
+
+        $userBegginer = factory(App\User::class)->create([
+            'email'=>'begginer@example.com',
+            'password' => bcrypt('begginerdemo')
+        ]);
+
+        $problems = Problem::take(10)->get();
+        $userBegginer->solve($problems[0],$problems[0]->answer);
+        
+    }
+}
